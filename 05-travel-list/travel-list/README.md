7f7092bc4ce41eded9703770c389858500b3924e
quietLearner <you@example.com>
2024-04-23 23:48:05 -0400

    complete this app

--- 05-travel-list/travel-list/README.md
+++ 05-travel-list/travel-list/README.md
@@ -1,1 +1,9 @@
 npx create-react-app@5 travel-list
+
+local state:
+used by one or few components.
+passing via props(parent to child)
+
+global state:
+context api
+redux
--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,16 +1,10 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 function Logo() {
   return <h1>🌴Far Away🧳</h1>;
 }
 
-function Form({ items }) {
+function Form({ items, onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -20,6 +14,8 @@ function Form({ items }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+    // console.log(newItem);
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -49,32 +45,77 @@ function Form({ items }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({
+  items,
+  handleDeleteItem,
+  handleToggleItem,
+  handleClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={handleClearList}>Clear list</button>
+      </div>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onToggleItem, onDeleteItem }) {
   return (
-    <li key={item.id}>
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description} <button>❌</button>
+        {item.quantity} {item.description}
       </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPackedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPackedItems / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼You have x items on your list, and you already packed X (X%)</em>
+      <em>
+        💼You have {numItems} items on your list, and you already packed{" "}
+        {numPackedItems}({percentage}%)
+      </em>
     </footer>
   );
 }
@@ -109,9 +150,14 @@ function App() {
   return (
     <div className="app">
       <Logo />
-      <Form items={items} />
-      <PackingList tems={items} />
-      <Stats />
+      <Form items={items} onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        handleDeleteItem={handleDeleteItem}
+        handleToggleItem={handleToggleItem}
+        handleClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
